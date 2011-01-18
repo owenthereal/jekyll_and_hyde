22c95b908818557051eaa487287ad24b108afe03
Jingwen Owen Ou <jingweno@gmail.com>
2011-01-17 16:58:44 -0800

    Removed group_task.rb.

--- lib/jekyll_and_hyde.rb
+++ lib/jekyll_and_hyde.rb
@@ -1,10 +1,11 @@
 require 'rubygems'
 require 'thor'
 require 'thor/group'
+require 'thor/runner'
 
 require 'jekyll_and_hyde/core_ext/string'
 
 require 'jekyll_and_hyde/version'
-require 'jekyll_and_hyde/group_task'
+require 'jekyll_and_hyde/runner'
 require 'jekyll_and_hyde/generators'
 require 'jekyll_and_hyde/generators/new'
