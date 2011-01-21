97b6e06699520dc964f990595783bae6dc0e449b
Jingwen Owen Ou <jingweno@gmail.com>
2011-01-21 11:33:57 -0800

    Added feature step for new command.

--- features/step_definitions/jekyll_and_hyde_steps.rb
+++ features/step_definitions/jekyll_and_hyde_steps.rb
@@ -1,10 +1,21 @@
+require 'fakefs/safe'
+
+Before do
+  FakeFS.activate!
+end
+
+After do
+  FakeFS.deactivate!
+end
+
 Given /^the jekyll_and_hyde gem installed$/ do
 end
 
 When /^I run the "jh ([^"]*) ([^"]*)" command$/ do |command, parameters|
-  JekyllAndHyde::Generators.invoke(command, parameters)
+  @command = JekyllAndHyde::Generators.find_task_class(command).new(parameters.split)
+  @command.invoke("jekyll_and_hyde:generators:#{command}")
 end
 
 Then /^I should have a folder named "([^"]*)" created$/ do |dir_name|
-  pending # express the regexp above with the code you wish you had
+  File.should be_directory(dir_name)
 end
--- lib/jekyll_and_hyde/generators.rb
+++ lib/jekyll_and_hyde/generators.rb
@@ -1,26 +1,17 @@
 module JekyllAndHyde
   module Generators
     class << self
-      def invoke(task_name, *args)
-        task = find_task_by(task_name)
-        task.invoke(task_to_invoke(task_name), args) unless task.nil?
-      end
-
-      def find_task_by(task_name)
+      def find_task_class(task_name)
         task_class = const_get(task_name.to_s.titlecase.to_sym)
         if !task_class.nil? && support_type?(task_class)
-          task_class.new
+          task_class
         else
           nil
         end
       end
 
       def support_type?(generator_class)
-        generator_class.ancestors.include?(GroupTask)
-      end
-
-      def task_to_invoke(task_name)
-        "jekyll_and_hyde:generators:#{task_name}"
+        generator_class.ancestors.include?(Thor::Base)
       end
     end
   end
